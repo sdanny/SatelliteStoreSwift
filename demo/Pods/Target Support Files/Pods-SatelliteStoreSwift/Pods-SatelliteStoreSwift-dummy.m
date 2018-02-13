5fa4a2abc3e820929d2146399918ddfbf92c65e9
Daniyar Salakhutdinov <daniyar_salakhutdinov@moneta.com>
2018-02-13 15:11:29 +0300

    Installed testing pods.

---
+++ demo/Pods/Target Support Files/Pods-SatelliteStoreSwift/Pods-SatelliteStoreSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SatelliteStoreSwift : NSObject
+@end
+@implementation PodsDummy_Pods_SatelliteStoreSwift
+@end
---
+++ demo/Pods/Target Support Files/Pods-SatelliteStoreSwiftTests/Pods-SatelliteStoreSwiftTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SatelliteStoreSwiftTests : NSObject
+@end
+@implementation PodsDummy_Pods_SatelliteStoreSwiftTests
+@end
